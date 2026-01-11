e361411abbc3a0a1ddf9f3c9cfa3c0113ddabae2
Carlota Reyes de Federico <reyesdefedericocarlota001.alu@stemgranada.com>
2026-01-11 10:55:03 +0100

    Subo ejercicio7 modificado

--- U4/U1/Relacion3/ejercicio7.java
+++ U4/U1/Relacion3/ejercicio7.java
@@ -7,25 +7,24 @@ public class ejercicio7 {
         Scanner scanner = new Scanner(System.in);
         double numero1, numero2;
         String operador;
-        double resultado;
-
+       
         System.out.println("Introduzca el primer número");
-        numero1 = scanner.nextInt();
+        numero1 = scanner.nextDouble();
         System.out.println("Introduzca el segundo número");
-        numero2 = scanner.nextInt();
+        numero2 = scanner.nextDouble();
         System.out.println("Introduzca el operador (+, -, *, /)");
         operador = scanner.next();
         scanner.close();
 
         if (operador.equals("+")) {
-            resultado = numero1 + numero2;
+            System.out.println(numero1 + numero2);
         } else if (operador.equals("-")) {
-            resultado = numero1 - numero2;
+            System.out.println(numero1 - numero2);
         } else if (operador.equals("*")) {
-            resultado = numero1 * numero2;
+            System.out.println(numero1 * numero2);
         } else if (operador.equals("/")) {
             if (numero2 != 0) {
-                resultado = numero1 / numero2;
+                System.out.println(numero1 / numero2);
             } else {
                 System.out.println(("No es posible dividir entre 0"));
             }
@@ -36,40 +35,3 @@ public class ejercicio7 {
     }
 }
 
-// <script>
-// let resultado
-
-// let numero1 = prompt("Introduzca un número")
-// numero1 = Number(numero1)
-
-// let numero2 = prompt("Introduzca otro número")
-// numero2 = Number(numero2)
-
-// let operador = prompt("Introduzca el operador (+, -, *, /)")
-// let division0
-
-// if (operador == "+") {
-// resultado = numero1 + numero2
-// } else if (operador == "-") {
-// resultado = numero1 - numero2
-// } else if (operador == "*") {
-// resultado = numero1 * numero2
-// } else if (operador == "/") {
-// if (numero2 != 0) {
-// resultado = numero1 / numero2
-// } else {
-// console.log("No es posible dividir entre 0");
-// division0 = true
-// }
-// } else {
-// console.log("Operador incorrecto")
-// }
-
-// if (!division0){
-// console.log("El resultado de " + numero1 + operador + numero2 + " es " +
-// resultado)
-// }else{
-// console.log("Vuelva a probar con valores correctos");
-
-// }
-// </script>
