c2efdf386e4841d26fb6741b9dd7d914b6dac493
Carlota Reyes de Federico <reyesdefedericocarlota001.alu@stemgranada.com>
2026-01-15 08:44:46 +0100

    Ejercicio6.java

--- U4/U1/Relacion6/ejercicio6.java
+++ U4/U1/Relacion6/ejercicio6.java
@@ -58,34 +58,3 @@ public class ejercicio6 {
         System.out.println("Programa finalizado.");
     }
 }
-// while (numero1 != "SALIR" && numero2 != "SALIR" && operacionUsuario !=
-// "SALIR") {
-// numero1 = prompt("Introduce el primer número:")
-
-// if (numero1 != "SALIR") {
-// numero1 = Number(numero1)
-
-// numero2 = prompt("Introduce el segundo número:")
-// if (numero2 != "SALIR") {
-// numero2 = Number(numero2)
-
-// operacionUsuario = prompt("Introduce la operación:")
-
-// if (operacionUsuario != "SALIR") {
-
-// if (operacionUsuario == "+") {
-// resultadoOperacion = numero1 + numero2
-// } else if (operacionUsuario == "-") {
-// resultadoOperacion = numero1 - numero2
-// } else if (operacionUsuario == "*") {
-// resultadoOperacion = numero1 * numero2
-// } else if (operacionUsuario == "/") {
-// resultadoOperacion = numero1 / numero2
-// }
-
-// alert("El resultado de " + numero1 + operacionUsuario + numero2 + " es " +
-// resultadoOperacion)
-// }
-// }
-// }
-// }
