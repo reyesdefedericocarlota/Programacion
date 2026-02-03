f71c4f6bfcc04f871247ee90c382ee5e88f09a1b
Carlota Reyes de Federico <reyesdefedericocarlota001.alu@stemgranada.com>
2026-02-03 19:33:37 +0100

    Ejercicio terminado TractoMatch.java

--- U4/RelacionRepaso/TractoMatch.java
+++ U4/RelacionRepaso/TractoMatch.java
@@ -63,11 +63,12 @@ public class TractoMatch {
     }
 
     // Validar índice
-    public static int validarIndice(Scanner scanner, int min, int parametroDefecto, ArrayList<Integer> listaTractores) {
+    public static int validarIndice(Scanner scanner, int min, int parametroDefecto, String mensaje,
+            ArrayList<Integer> listaTractores) {
         int indice;
         do {
             try {
-                mostrarMensaje("Introduce el índice del tractor que desea evaluar:");
+                mostrarMensaje(mensaje);
                 indice = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
                 mostrarMensaje("Error, debes introducir un número.");
@@ -99,6 +100,20 @@ public class TractoMatch {
         }
     }
 
+    // Eliminar tractor
+    public static String eliminarTractor(
+            int indiceEliminado,
+            ArrayList<String> listaMarcas,
+            ArrayList<Integer> listaPotencias,
+            ArrayList<String> listaTrabajos) {
+
+        String marcaEliminada = listaMarcas.remove(indiceEliminado);
+        listaPotencias.remove(indiceEliminado);
+        listaTrabajos.remove(indiceEliminado);
+
+        return marcaEliminada;
+    }
+
     // Mostrar mensaje
     public static void mostrarMensaje(String mensaje) {
         System.out.println(mensaje);
@@ -120,56 +135,70 @@ public class TractoMatch {
                 "2 = Listar tractores\n" +
                 "3 = Evaluar tractor\n" +
                 "4 = Eliminar tractor";
+        final String MENSAJEOPCION3 = "Introduce el índice del tractor que desea evaluar:";
+        final String MENSAJEOPCION4 = "Introduce el índice del tractor que desea eliminar:";
         int elegirOpcion;
         String marcaTractor;
         int potenciaTractor;
         String trabajoTractor;
         int indice;
         int potenciaMinima;
+        String tractorEliminado;
 
         do {
             elegirOpcion = opcionMenu(scanner, MENU, 0, 4, -1);
 
-            if (elegirOpcion == 1) {
-                System.out.println("Introduce la marca del tractor:");
-                marcaTractor = scanner.nextLine();
-                marcas.add(marcaTractor);
+            if ((elegirOpcion == 2 || elegirOpcion == 3 || elegirOpcion == 4) && marcas.isEmpty()) {
+                System.out.println("No hay tractores registrados. Añade uno primero.");
+            } else {
 
-                potenciaTractor = solicitarPotencia(scanner, 0, -1);
-                potencias.add(potenciaTractor);
+                if (elegirOpcion == 1) {
+                    System.out.println("Introduce la marca del tractor:");
+                    marcaTractor = scanner.nextLine();
+                    marcas.add(marcaTractor);
 
-                trabajoTractor = validarTrabajo(scanner, validarTrabajos);
-                trabajos.add(trabajoTractor);
+                    potenciaTractor = solicitarPotencia(scanner, 0, -1);
+                    potencias.add(potenciaTractor);
 
-            } else if (elegirOpcion == 2) {
-                for (int i = 0; i < marcas.size(); i++) {
-                    System.out.println(i + " -> Marca: " + marcas.get(i) + " | " + potencias.get(i) + "CV | trabajo: "
-                            + trabajos.get(i));
+                    trabajoTractor = validarTrabajo(scanner, validarTrabajos);
+                    trabajos.add(trabajoTractor);
+
+                } else if (elegirOpcion == 2) {
+                    for (int i = 0; i < marcas.size(); i++) {
+                        System.out
+                                .println(i + " -> Marca: " + marcas.get(i) + " | " + potencias.get(i) + "CV | trabajo: "
+                                        + trabajos.get(i));
+                    }
+
+                } else if (elegirOpcion == 3) {
+                    indice = validarIndice(scanner, 0, -1, MENSAJEOPCION3, potencias);
+
+                    // Mostrar tractor evaluado
+                    System.out.println("Tractor evaluado: " + marcas.get(indice) + " | "
+                            + potencias.get(indice) + "CV | trabajo: " + trabajos.get(indice));
+
+                    // Determinar la potencia mínima según el trabajo
+                    if (trabajos.get(indice).equals("arado")) {
+                        potenciaMinima = 120;
+                    } else if (trabajos.get(indice).equals("transporte")) {
+                        potenciaMinima = 90;
+                    } else { // "siembra"
+                        potenciaMinima = 70;
+                    }
+
+                    // Evaluar
+                    calcularApto(potencias, indice, potenciaMinima);
+
+                } else if (elegirOpcion == 4) {
+                    indice = validarIndice(scanner, 0, -1, MENSAJEOPCION4, potencias);
+
+                    tractorEliminado = eliminarTractor(indice, marcas, potencias, trabajos);
+                    System.out.println("El tractor: " + tractorEliminado + " ha sido eliminado.");
+
                 }
-
-            } else if (elegirOpcion == 3) {
-
-                indice = validarIndice(scanner, 0, -1, potencias);
-
-                // Mostrar tractor evaluado
-                System.out.println("Tractor evaluado: " + marcas.get(indice) + " | "
-                        + potencias.get(indice) + "CV | trabajo: " + trabajos.get(indice));
-
-                // Determinar la potencia mínima según el trabajo
-                if (trabajos.get(indice).equals("arado")) {
-                    potenciaMinima = 120;
-                } else if (trabajos.get(indice).equals("transporte")) {
-                    potenciaMinima = 90;
-                } else { // "siembra"
-                    potenciaMinima = 70;
-                }
-
-                // Evaluar
-                calcularApto(potencias, indice, potenciaMinima);
-
-            } else if (elegirOpcion == 4) {
-                
             }
         } while (elegirOpcion != 0);
+
+        System.out.println("Ha salido del programa");
     }
 }
